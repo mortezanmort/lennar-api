068d39d011a543a0e28c92f4544c99632dfa2861
mortezanmort <mortezanmort@gmail.com>
2024-07-03 14:54:03 +0500

    create stuffs app

--- config/settings.py
+++ config/settings.py
@@ -66,6 +66,7 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     # custom apps go here
+    "stuffs.apps.StuffsConfig",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
